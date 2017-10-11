4cb476bda65c22911e74561c5adf0b2caef49a9c
tianyawy <tianya_wy@hotmail.com>
2017-10-11 23:50:17 +0800

    [victoryw] #n/a init

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker run -d \
+-p 8500:8500 \
+--name=dev-consul consul
---
+++ src/main/java/org/victoryw/springcloudexample/springcloudexample/SpringCloudExampleApplication.java
@@ -0,0 +1,12 @@
+package org.victoryw.springcloudexample.springcloudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudExampleApplication.class, args);
+	}
+}
