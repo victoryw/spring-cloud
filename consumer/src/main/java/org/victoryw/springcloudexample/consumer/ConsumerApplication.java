7846b496ce5f34af8b095142051fdd64daecd6d1
tianyawy <tianya_wy@hotmail.com>
2017-10-15 23:23:07 +0800

    [victoryw] #2 use the feign

--- consumer/src/main/java/org/victoryw/springcloudexample/consumer/ConsumerApplication.java
+++ consumer/src/main/java/org/victoryw/springcloudexample/consumer/ConsumerApplication.java
@@ -5,20 +5,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableDiscoveryClient
 @RestController
+@EnableFeignClients
 @SpringBootApplication
 public class ConsumerApplication {
 
-    private static String appName = "testProducerApp";
-
+    private static final String appName = "testProducerApp";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -29,13 +32,30 @@ public class ConsumerApplication {
         return new RestTemplate();
     }
 
+    @Autowired
+    private SampleClient sampleClient;
+
+
 
     @RequestMapping("/rest")
     public String rest() {
         return this.restTemplate.getForObject("http://"+appName+"/hello-world", String.class);
     }
 
+    @RequestMapping("/feign")
+    public String feign(){
+        return this.sampleClient.choose();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
+
+}
+
+@FeignClient("testProducerApp")
+interface SampleClient {
+
+    @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+    String choose();
 }
